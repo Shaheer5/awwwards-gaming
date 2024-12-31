6f081cedc51698cd31ea40913b5d30e495e876c0
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-12-31 11:03:55 +0500

    update footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium, FaGithub } from "react-icons/fa";
+import { FaTwitter, FaMedium, FaGithub, FaGlobe } from "react-icons/fa";
 
 const socialLinks = [
-  { href: "https://discord.com/", icon: <FaDiscord /> },
+  { href: "https://shaheermansoor.com/", icon: <FaGlobe /> },
   { href: "https://twitter.com/shaheermansoor2", icon: <FaTwitter /> },
-  { href: "https://youtube.com", icon: <FaYoutube /> },
   { href: "https://github.com/Shaheer5", icon: <FaGithub /> },
   { href: "https://medium.com/@shaheermansoor321", icon: <FaMedium /> },
 ];
@@ -12,9 +11,7 @@ const Footer = () => {
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
-        <p className="text-center text-sm font-light md:text-left">
-          ©Shaheer Mansoor 2024. All rights reserved
-        </p>
+        <p className="text-center text-sm font-light md:text-left">©Shaheer Mansoor 2024. All rights reserved</p>
 
         <div className="flex justify-center gap-4  md:justify-start">
           {socialLinks.map((link, index) => (
@@ -30,10 +27,7 @@ const Footer = () => {
           ))}
         </div>
 
-        <a
-          href="#privacy-policy"
-          className="text-center text-sm font-light hover:underline md:text-right"
-        >
+        <a href="#privacy-policy" className="text-center text-sm font-light hover:underline md:text-right">
           Privacy Policy
         </a>
       </div>
